58c67f5ab1a44e301d76be3735222aee6ffb89f8
NguyenDoan85 <ericdoan2008@gmail.com>
2022-07-01 11:23:41 -0700

    fix event listener, add lines to html

--- assets/js/randomcocktail.js
+++ assets/js/randomcocktail.js
@@ -1,14 +1,13 @@
 // Global variable
-const searchCocktail = document.getElementById('cocktail');
+var randomClick = document.getElementById('randomBt');
 
-// Add event listener to 
-searchCocktail.addEventListener('change', () => {
-    const searchValue = searchCocktail.value;
-    getCocktailByName(searchValue);
-})
+ //Event listener to random button
+randomClick.addEventListener("click", function() {
+    getRandomCocktail();
+  });
 
 // function for random cocktail by request through the API
-function randomCocktail() {
+function getRandomCocktail() {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
         .then((response) => response.json())
         .then((cocktailData) => {
@@ -17,29 +16,29 @@ function randomCocktail() {
         .catch((error) => {
             console.log(error)
         })
-}
+};
 
 //function to display random cocktail in the html
 function displayRandomCocktail(cocktailData) {
     const { drinks } = cocktailData;
-    const cocktailpictureElement = document.getElementById('cocktailPicture');
+    const cocktailPicEl = document.getElementById('cocktailPicture');
     let cocktailPicture = `<img class="drinkThumb" src= "${drinks[0].strDrinkThumb}">`;
 
-    cocktailpictureElement.innerHTML = cocktailPicture;
+    cocktailPicEl.innerHTML = cocktailPicture;
 
     //display title
-    const cocktailTitleElement = document.getElementById('cocktailTitle');
+    const cocktailTitleEl = document.getElementById('cocktailTitle');
 
     let cocktailInfo = `
     <h4 class="cocktailName"> ${drinks[0].strDrink} </h4>
     <p class="typeOfglass">Type of glass:</p><p>${drinks[0].strGlass}</p>
     </br><p class="howToMake"></p>`
 
-    cocktailTitleElement.innerHTML = cocktailInfo;
+    cocktailTitleEl.innerHTML = cocktailInfo;
 
 
     //display ingredients
-    const cocktailInfoElement = document.getElementById('howTo');
+    const cocktailInfoEl = document.getElementById('howTo');
 
     let result = '';
     for (var i = 1; i <= 15; i++) {
@@ -48,16 +47,16 @@ function displayRandomCocktail(cocktailData) {
         if ((drinks[0][measures]) && (drinks[0][ingridients]) !== "") {
             result = result + ` <p>${drinks[0][measures]} ${drinks[0][ingridients]}</p>`;
 
-            cocktailInfoElement.innerHTML = result;
+            cocktailInfoEl.innerHTML = result;
         };
-    }
-
+    };  
 
     //display instructions
-    const cocktailInstructionsElement = document.getElementById('instructions');
+    const cocktailInstEl = document.getElementById('instructions');
     let instructions = `
     </br><p>${drinks[0].strInstructions}</p>`;
 
-    cocktailInstructionsElement.innerHTML = instructions;
+    cocktailInstEl.innerHTML = instructions;
 
-}
+};
+
